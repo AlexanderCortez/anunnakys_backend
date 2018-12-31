5131731bbf2fc34a1ff857cd8783908dfe94301e
alexandercortez <joelalexander10@outlook.com>
2018-12-30 20:17:19 -0600

    Adding event controller

--- app/controllers/event_controller.js
+++ app/controllers/event_controller.js
@@ -1,7 +1,49 @@
 const router = require('express').Router();
 
+const Event = require('../models/event_model');
+
 const showEvents = (req, res) => {
-  
+  Event
+    .fetchAll()
+    .then((events) => {
+      res.send({
+        events,
+      });
+    })
+    .catch((err) => {
+      res.status(500).send({
+        error: err.message || 'Error retrieving events',
+      });
+    });
+};
+
+const createEvent = (req, res) => {
+  const {
+    name, place, npc, start, finish,
+  } = req.body;
+  Event
+    .forge({
+      event_name: name,
+      event_place: place,
+      event_npc: npc,
+      event_start: start,
+      event_finish: finish,
+    })
+    .save(null, { method: 'insert' })
+    .then((event) => {
+      res.send({
+        message: 'Event creared successfully',
+        event,
+      });
+    })
+    .catch((err) => {
+      res.status(500).send({
+        error: err.message || 'Error adding event',
+      });
+    });
 };
 
 router.get('/', showEvents);
+router.post('/', createEvent);
+
+module.exports = router;
