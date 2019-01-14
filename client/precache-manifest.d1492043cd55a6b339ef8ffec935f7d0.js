7417e035881bb9519314af8a310278cac6f3392b
alexandercortez <joelalexander10@outlook.com>
2019-01-14 16:21:02 -0600

    Adding new changes

---
+++ client/precache-manifest.d1492043cd55a6b339ef8ffec935f7d0.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "0a0ba94e1957a6d5523c",
+    "url": "/static/js/main.0a0ba94e.chunk.js"
+  },
+  {
+    "revision": "135e615e45284b7222bf",
+    "url": "/static/js/1.135e615e.chunk.js"
+  },
+  {
+    "revision": "0a0ba94e1957a6d5523c",
+    "url": "/static/css/main.6661df85.chunk.css"
+  },
+  {
+    "revision": "135e615e45284b7222bf",
+    "url": "/static/css/1.7ce7655f.chunk.css"
+  },
+  {
+    "revision": "a3f8e8751fbd9c62136d021d810a59e0",
+    "url": "/index.html"
+  }
+];
--- client/service-worker.js
+++ client/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.7f35e16076140e16bbc5652cf3c84531.js"
+  "/precache-manifest.d1492043cd55a6b339ef8ffec935f7d0.js"
 );
 
 workbox.clientsClaim();
